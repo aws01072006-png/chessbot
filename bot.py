bfb16b51b8f3815503ac67fd17ade8849e767f06
aws01072006-png <aws01072006@gmail.com>
2026-02-01 13:56:58 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,10 +8,7 @@ class ChessBot:
         pass
 
     def get_move(self, board: ChessBoard):
-        """
-        Given the current board state, returns the chosen move.
-        This is the main function students will implement.
-        """
+       
         legal_moves = board.get_legal_moves()
         if legal_moves:
             return random.choice(legal_moves)
