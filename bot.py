a6fed2b950a1b04e91e40b5d7da86aabf6a41a2a
aws01072006-png <aws01072006@gmail.com>
2026-02-24 09:48:45 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,8 +8,9 @@ class ChessBot:
         pass
 
     def get_move(self, board: ChessBoard):
-       
         legal_moves = board.get_legal_moves()
+        board_state = board.get_board_state()
+        is_over = board.is_game_over()
         if legal_moves:
             return random.choice(legal_moves)
         return None
